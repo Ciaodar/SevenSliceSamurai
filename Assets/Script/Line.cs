3adb104baa0463ea6f603e5fbaa86263b5c1ac7c
Blofo <taylanadam123@gmail.com>
2024-04-21 12:52:42 +0300

    AAAAAAAAA

--- Assets/Script/Line.cs
+++ Assets/Script/Line.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Line : MonoBehaviour
 {
+    public Camera myCam;
     public TrailRenderer thisTrail;
     public CameraShake1 camShake;
+    public ParticleSystem particleSystem;
 
     void Update()
     {
@@ -14,20 +16,21 @@ public class Line : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             thisTrail.Clear();
-            Vector3 mypoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 mypoint = myCam.ScreenToWorldPoint(Input.mousePosition);
             transform.position = new Vector3(mypoint.x, mypoint.y, -5f);
             thisTrail.Clear();
             camShake.shakeDuration = 0.5f;
         }
         else if (Input.GetKey(KeyCode.Mouse0))
         {
-            Vector3 mypoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 mypoint = myCam.ScreenToWorldPoint(Input.mousePosition);
+            particleSystem.Emit(2);
             transform.position = new Vector3(mypoint.x, mypoint.y, -5f);
             camShake.shakeDuration = 0.5f;
         }
         else if (Input.GetKeyUp(KeyCode.Mouse0))
         {
-            Vector3 mypoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 mypoint = myCam.ScreenToWorldPoint(Input.mousePosition);
 
             transform.position = new Vector3(mypoint.x, mypoint.y, -5f);
             camShake.shakeDuration = 0.02f;
