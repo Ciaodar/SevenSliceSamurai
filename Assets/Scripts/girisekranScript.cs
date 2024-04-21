2cd1ac22fbdfd6534d9dfaeb98e5ca4687db2f81
Emre Kovancı <78854456+Emr1k@users.noreply.github.com>
2024-04-21 16:24:03 +0300

    main menu 1

---
+++ Assets/Scripts/girisekranScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class girisekranScript : MonoBehaviour
+{
+    public GameObject img;
+
+    public void Ac()
+    {
+        img.SetActive(true);
+    }
+}
